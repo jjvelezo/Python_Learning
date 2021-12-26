d8f0cafed1f2df84e83e0c463182e4fd3491cea8
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-12-26 12:58:40 -0500

    Create Multiplicacion.py

---
+++ Codes/Practica/Multiplicacion.py
@@ -0,0 +1,19 @@
+x = input()
+y = input()
+
+x = int(x)
+y = int(y)*2
+
+
+
+if(x == y):
+    print("Felicidaddes")
+    
+    
+    
+if(x < y):
+    print("X es menor")
+    
+    
+if(x > y):
+    print("X es mayor")
