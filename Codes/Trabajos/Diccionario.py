09cce3f7808f317f061dfd3cbf86863d11b8c7d1
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-12-26 12:51:18 -0500

    Create Diccionario.py

---
+++ Codes/Trabajos/Diccionario.py
@@ -0,0 +1,18 @@
+persona = {
+  "Name": "Pepito",
+  "Lastname": "Gomez",
+  "Age": 17
+}
+
+
+producto = {
+  "Type": "Book",
+  "Quantity": 50,
+  "Price": 4.99
+}
+
+print(dir(persona))
+print("-------------")
+print(persona)
+print(producto)
+print(persona.items())
