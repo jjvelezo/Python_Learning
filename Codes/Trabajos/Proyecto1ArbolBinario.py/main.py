11d22783fb6acf41eff78e2cd7c39a3fde31e1a3
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-12-26 12:47:27 -0500

    Create main.py

---
+++ Codes/Trabajos/Proyecto1ArbolBinario.py/main.py
@@ -0,0 +1,16 @@
+from arbolBinario import Arbol
+
+
+
+
+arbol_numeros = Arbol(4)
+arbol_numeros.agregar(6)
+arbol_numeros.agregar(3)
+arbol_numeros.agregar(1)
+arbol_numeros.agregar(90)
+arbol_numeros.agregar(8)
+dato2 = arbol_numeros.agregar(6)
+dato = arbol_numeros.agregar(15)
+
+
+arbol_numeros.inorden()
